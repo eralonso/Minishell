73211a5d9f781de87849e4db86e10ac5ba1a0048
Pol Ramos <pol.rm2004@gmail.com>
2023-05-04 13:42:21 +0200

    stdlib
    sip

--- src/minishell.c
+++ src/minishell.c
@@ -6,12 +6,12 @@
 /*   By: pramos-m <pramos-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 17:36:26 by eralonso          #+#    #+#             */
-/*   Updated: 2023/05/04 13:40:54 by pramos-m         ###   ########.fr       */
+/*   Updated: 2023/05/04 13:42:10 by pramos-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	<msh.h>
-#include	<stdio.h>
+#include	<stdlib.h>
 
 int	main(int ac, char **av, char **env)
 {
