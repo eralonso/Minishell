8dbaedc7ce9df74281fa2f6e18b189c62808a5b9
Pol Ramos <pol.rm2004@gmail.com>
2023-05-05 13:44:44 +0200

    includes

--- inc/msh.h
+++ inc/msh.h
@@ -3,14 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   msh.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eralonso <eralonso@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pramos-m <pramos-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 16:00:02 by eralonso          #+#    #+#             */
-/*   Updated: 2023/05/04 16:12:51 by eralonso         ###   ########.fr       */
+/*   Updated: 2023/05/05 13:44:16 by pramos-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MSH_H
 # define MSH_H
+
+# include   <stdlib.h>
+# include   <stdio.h>
+# include   <limits.h>
+# include   <errno.h>
+# include   <unistd.h>
+# include   <stdbool.h>
 
 #endif
