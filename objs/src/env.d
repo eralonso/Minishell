fff5832a47f20e8e6ece2a603f4648ef98edc411
Pol Ramos <pol.rm2004@gmail.com>
2023-05-10 13:22:27 +0200

    tu vida
    que lo habias subido con los objetos

--- objs/src/env.d
+++
@@ -1,4 +0,0 @@
-objs/src/env.o: src/env.c inc/msh.h lib/readline/readline.h \
-  lib/readline/rlstdc.h lib/readline/rltypedefs.h lib/readline/keymaps.h \
-  lib/readline/chardefs.h lib/readline/tilde.h lib/readline/history.h \
-  lib/er_lib/ft_printf/inc/ft_printf.h lib/er_lib/libft/libft.h
