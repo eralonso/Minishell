38c01e852fc7b53c840af24e46c1d6baf9fb63e7
Pol Ramos <pol.rm2004@gmail.com>
2023-05-11 13:49:25 +0200

    new&|check
    guapo

--- src/validate_input.c
+++ src/validate_input.c
@@ -3,48 +3,51 @@
 /*                                                        :::      ::::::::   */
 /*   validate_input.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eralonso <eralonso@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pramos-m <pramos-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 12:18:49 by pramos-m          #+#    #+#             */
-/*   Updated: 2023/05/11 10:58:03 by eralonso         ###   ########.fr       */
+/*   Updated: 2023/05/11 13:48:20 by pramos-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	<msh.h>
 
-
-int check_syntax(char *input, t_kof *fok)
+int	check_syntax(char *input, t_kof *fok)
 {
-    int i;
+	int	i;
 
-    i = -1;
-    while (input[++i])
-    {
-        ((input[i] == '\"') && (fok->sq < 0) && (fok->dq *= -1));
-        ((input[i] == '\'') && (fok->dq < 0) && (fok->sq *= -1));
-        ((input[i] == '(' && (fok->sq < 0 && fok->dq < 0)) && (fok->op++));
-        ((input[i] == ')' && (fok->sq < 0 && fok->dq < 0) && \
-            fok->op > fok->cp) && (fok->cp++));
-        ((ft_strnstr(&input[i], "&&\0", 2) && (fok->sq < 0 && fok->dq < 0) && \
-            (fok->and = 1)));
-    }
-    return (0);
+	i = -1;
+	while (input[++i])
+	{
+		((fok->sq < 0) && (input[i] == '\"') && (fok->dq *= -1));
+		((fok->dq < 0) && (input[i] == '\'') && (fok->sq *= -1));
+		((fok->sq < 0 && fok->dq < 0) && (input[i] == '(') && (fok->op++));
+		(((fok->sq < 0 && fok->dq < 0) && input[i] == ')' && \
+			fok->op > fok->cp) && (fok->cp++));
+		if ((fok->sq < 0 && fok->dq < 0) && \
+			input[i] == '&' && input[i + 1] != '&')
+			return (1);
+		if ((fok->sq < 0 && fok->dq < 0) && ft_strnstr(&input[i], "&&\0", 2) \
+			&& ft_strchr("&|\0", input[++i + 1]))
+			return (1);
+	}
+	return (0);
 }
 
-int validate_input(char *input)
+int	validate_input(char *input)
 {
-    t_kof   fok;
-    
-    fok.dq = -1;
-    fok.sq = -1;
-    fok.op = 0;
-    fok.cp = 0;
-    fok.and = 0;
-    fok.or = 0;
-    printf("%s\n", input);
-    if (check_syntax(input, &fok))
-        return (1);
-    // if (check_redirect(input));
-    //     return (1);
-    return (0);
+	t_kof	fok;
+
+	fok.dq = -1;
+	fok.sq = -1;
+	fok.op = 0;
+	fok.cp = 0;
+	fok.and = 0;
+	fok.or = 0;
+	printf("%s\n", input);
+	if (check_syntax(input, &fok))
+		return (1);
+	// if (check_redirect(input));
+	//     return (1);
+	return (0);
 }
